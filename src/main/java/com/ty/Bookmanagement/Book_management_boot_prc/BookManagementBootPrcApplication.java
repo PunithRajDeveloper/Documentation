56d4b5f385fa26de28f49ce527e9f8b0c28f7083
https://github.com/suhassg25 <sgsuhassg@gmail.com>
2022-12-07 09:47:35 +0530

    Adding Starter files

---
+++ src/main/java/com/ty/Bookmanagement/Book_management_boot_prc/BookManagementBootPrcApplication.java
@@ -0,0 +1,13 @@
+package com.ty.Bookmanagement.Book_management_boot_prc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementBootPrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementBootPrcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/Bookmanagement/Book_management_boot_prc/BookManagementBootPrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.Bookmanagement.Book_management_boot_prc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementBootPrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
