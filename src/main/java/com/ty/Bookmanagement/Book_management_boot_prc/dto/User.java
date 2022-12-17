36ce1f28305eb7133270193a8baca97990bfb684
PunithRajDeveloper <117797090+PunithRajDeveloper@users.noreply.github.com>
2022-12-17 12:11:51 +0530

    Update User.java

--- src/main/java/com/ty/Bookmanagement/Book_management_boot_prc/dto/User.java
+++ src/main/java/com/ty/Bookmanagement/Book_management_boot_prc/dto/User.java
@@ -2,11 +2,10 @@ package com.ty.Bookmanagement.Book_management_boot_prc.dto;
 
 import java.util.List;
 
-<<<<<<< refs/remotes/origin/rajbala
-=======
+
 import javax.persistence.CascadeType;
 import javax.persistence.Convert;
->>>>>>> local
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -25,22 +24,9 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	@NotNull(message = "enter name of the seller")
-    private String name;
+        private String name;
 	@NotNull(message = "Enter the present address")
-<<<<<<< refs/remotes/origin/rajbala
-    private String address;
-    @NotNull(message = "Enter an active phone number to contact")
-    private long phonNum;
-    private String email;
-    private String gender;
-    
-    
-    @OneToMany
-    private List<Book> books;   
-	
-	
-=======
-	private String address;
+	private String address;	
 	@NotNull(message = "Enter an active phone number to contact")
 	private long phonNum;
 	@Convert(converter = AesEncryption.class)
@@ -50,5 +36,4 @@ public class User {
 	@OneToMany(cascade = CascadeType.REMOVE)
 	private List<Book> books;
 
->>>>>>> local
 }
