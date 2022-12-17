323a8123b11ca3dd2b1868628d0ac6fbdac94bc2
unknown <rajbala0.thakur@gmail.com>
2022-12-17 12:05:33 +0530

    User Encryption is done

--- src/main/java/com/ty/Bookmanagement/Book_management_boot_prc/dto/User.java
+++ src/main/java/com/ty/Bookmanagement/Book_management_boot_prc/dto/User.java
@@ -2,12 +2,19 @@ package com.ty.Bookmanagement.Book_management_boot_prc.dto;
 
 import java.util.List;
 
+<<<<<<< refs/remotes/origin/rajbala
+=======
+import javax.persistence.CascadeType;
+import javax.persistence.Convert;
+>>>>>>> local
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+
+import com.ty.Bookmanagement.Book_management_boot_prc.util.AesEncryption;
 
 import lombok.Data;
 
@@ -20,6 +27,7 @@ public class User {
 	@NotNull(message = "enter name of the seller")
     private String name;
 	@NotNull(message = "Enter the present address")
+<<<<<<< refs/remotes/origin/rajbala
     private String address;
     @NotNull(message = "Enter an active phone number to contact")
     private long phonNum;
@@ -31,4 +39,16 @@ public class User {
     private List<Book> books;   
 	
 	
+=======
+	private String address;
+	@NotNull(message = "Enter an active phone number to contact")
+	private long phonNum;
+	@Convert(converter = AesEncryption.class)
+	private String email;
+	private String gender;
+
+	@OneToMany(cascade = CascadeType.REMOVE)
+	private List<Book> books;
+
+>>>>>>> local
 }
